ddf2117b4ede9ea1225ddb052e7bdfa58b9d67ce
Harsh Singal <harsh.singal@gmail.com>
2022-01-07 10:48:25 +1100

    todolist component implemented

---
+++ frontend/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,14 @@
+import './TodoList.css'
+
+const TodoList = () => {
+  return (
+    <div className='todoList'>
+      <div className="todoList__container">
+      TodoList
+      </div>
+      
+    </div>
+  )
+}
+
+export default TodoList
