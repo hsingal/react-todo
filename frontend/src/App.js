94400bc041fc00a76c0249f418ac96fa5c9879ae
Harsh Singal <harsh.singal@gmail.com>
2022-01-07 15:58:33 +1100

    post a new todo and display in list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
+import { useState } from 'react';
 import './App.css'
 import TodoForm from './components/TodoForm/TodoForm';
 import TodoList from './components/TodoList/TodoList';
 import data from './data/data';
 
 function App() {
-  const todoItemsList = data;
+  const [todoList, setTodoList] = useState([])
   return (
     <div className="App">
-      <TodoForm />
-      <TodoList todoItemsList={todoItemsList}/>
+      <TodoForm todoList={todoList} setTodoList={setTodoList}/>
+      <TodoList todoItemsList={todoList}/>
     </div>
   );
 }
--- frontend/src/components/TodoForm/TodoForm.jsx
+++ frontend/src/components/TodoForm/TodoForm.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import "./TodoForm.css";
 
-const TodoForm = () => {
+const TodoForm = ({ todoList, setTodoList }) => {
+  const [inputText, setInputText] = useState("");
+
+  const handleInputText = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    setTodoList([
+      ...todoList,
+      { id: new Date(Date.now()).getTime(), desc: inputText },
+    ]);
+    setInputText("");
+  };
   return (
     <div className="todoForm">
       <div className="todoForm__container">
         <input
+          value={inputText}
           className="todoForm__container--inputField"
           type="text"
           placeholder="Enter your todo task"
+          onChange={handleInputText}
         />
-        <button className="todoForm__container--button">Submit</button>
+        <button
+          className="todoForm__container--button"
+          onClick={handleButtonClick}
+        >
+          Submit
+        </button>
       </div>
     </div>
   );
