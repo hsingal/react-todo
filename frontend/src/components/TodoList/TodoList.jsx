7905428f34107685efbda76118f697d1d6493d28
Harsh Singal <harsh.singal@gmail.com>
2022-01-07 12:21:47 +1100

    populate sample data in todolist and fix display when todolist is empty

--- frontend/src/components/TodoList/TodoList.jsx
+++ frontend/src/components/TodoList/TodoList.jsx
@@ -1,22 +1,27 @@
-import TodoListItem from '../TodoListItem/TodoListItem'
-import './TodoList.css'
+import TodoListItem from "../TodoListItem/TodoListItem";
+import "./TodoList.css";
 
-const TodoList = () => {
-  return (
-    <div className='todoList'>
-      <div className="todoList__container">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+const TodoList = ({ todoItemsList }) => {
+  console.log(`${todoItemsList.length}`);
+  if (`${todoItemsList.length}` === '0') {
+    return (
+      <div className="todoList">
+        <div className="todoList__container--empty">
+          No todos at this time!
+        </div>
       </div>
-      
-    </div>
-  )
-}
+    )
+  } else {
+    return (
+      <div className="todoList">
+        <div className="todoList__container">
+          {todoItemsList.map((item) => (
+            <TodoListItem key={item.id} todoItem={item.desc} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+};
 
-export default TodoList
+export default TodoList;
