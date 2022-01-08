ad1b5b7ef83016aab54e004f61552464d9e8b2ed
Harsh Singal <harsh.singal@gmail.com>
2022-01-08 12:25:35 +1100

    delete todo from list

--- frontend/src/components/TodoListItem/TodoListItem.jsx
+++ frontend/src/components/TodoListItem/TodoListItem.jsx
@@ -1,13 +1,22 @@
 import "./TodoListItem.css";
 
-const TodoListItem = ({ todoItem }) => {
+const TodoListItem = ({ todoItem, onDeleteTodoItem }) => {
+  const handleDeleteButton = (e) => {
+    e.preventDefault();
+    onDeleteTodoItem(todoItem);
+  };
   return (
     <div className="todoListItem">
       <div className="todoListItem__wrapper">
         <label className="todoListItem__wrapper--label">{todoItem.desc}</label>
         <div className="todoListItem__wrapper--buttons">
           <button className="todoListItem__wrapper--edit">Edit</button>
-          <button className="todoListItem__wrapper--delete">Delete</button>
+          <button
+            className="todoListItem__wrapper--delete"
+            onClick={handleDeleteButton}
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
